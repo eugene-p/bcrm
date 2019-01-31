5547056878cc7dc1e844f4a07ca53f74748c793b
Joyeux <joyeuxa@corp.advancedsolutions.eds.com>
2019-01-30 16:21:38 -0800

    add comments

--- src/app/decorators/rest.py
+++ src/app/decorators/rest.py
@@ -1,10 +1,24 @@
 
+# coding: utf-8
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
+
+"""
+    Rest Validator
+    ========================
+    Contains validation for
+        - json payload
+        - payload schema validation
+"""
 from functools import wraps
 from flask import jsonify, request, make_response
 from jsonschema import validate, ValidationError
 
 
 def validate_payload_is_json(func):
+    """
+    Validates that the request content type is json
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         if not request.json:
@@ -14,6 +28,9 @@ def validate_payload_is_json(func):
     return wrapper
 
 def validate_payload_with_schema(schema):
+    """
+    Validates that the payload is json
+    """
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
